f8d2e28fdb93f60ed058eff0ef233c2c6df5953f
John Azzaro <jazzaro@temple.edu>
2020-03-31 21:46:00 -0700

    updated TheDate

--- state-test/src/state/TheDate.js
+++ state-test/src/state/TheDate.js
@@ -4,9 +4,12 @@ class TheDate extends React.Component {
   constructor(props) {
     super(props)
     this.state = { datetime: new Date() };
+    console.log('constructor')
   }
   componentDidMount() {
+    console.log('componentDidMount')
     this.interval = setInterval(() => {
+      console.log('setInterval')
       this.setState({
         datetime: new Date()
       })
@@ -16,6 +19,7 @@ class TheDate extends React.Component {
     clearInterval(this.interval)
   }
   render() {
+    console.log('render')
     return (
       <div>{this.state.datetime.toLocaleString()}</div>
     )
