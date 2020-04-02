4656fc1758998ebf03db54df510d4bf4f5e8a872
John Azzaro <jazzaro@temple.edu>
2020-04-01 22:46:31 -0700

    handler error

--- state-test/src/state/Counter.js
+++ state-test/src/state/Counter.js
@@ -4,11 +4,14 @@ class Counter extends React.Component {
   state = {
     count: 0
   };
+  handleButtonClick() {
+    console.log(this.state.count)
+  }
   render() {
     return (
       <div>
         <p>The current count: {this.state.count}</p>
-        <button>
+        <button onClick={this.handleButtonClick}>
           Add 1
         </button>
       </div>
