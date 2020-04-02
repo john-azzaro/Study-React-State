a687401bb7d957cdbface8f1ca583af3a9226b72
John Azzaro <jazzaro@temple.edu>
2020-04-01 23:05:24 -0700

    updated handler

--- state-test/src/state/Counter.js
+++ state-test/src/state/Counter.js
@@ -4,14 +4,14 @@ class Counter extends React.Component {
   state = {
     count: 0
   };
-  handleButtonClick() {
+  handleButtonClick = () => {
     console.log(this.state.count)
   }
   render() {
     return (
       <div>
         <p>The current count: {this.state.count}</p>
-        <button onClick={() => this.handleButtonClick()}>
+        <button onClick={this.handleButtonClick}>
           Add 1
         </button>
       </div>
